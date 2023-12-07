261f0295723ef803315ffa20cc84536ca607306f
sajjadGh1995@hotmail.com <sajjadGh1995@hotmail.com>
2023-12-07 11:13:34 +0330

    Add Folders OF Architecture And Add Leave Classes

---
+++ Solution1/HR_Management.Domain/LeaveType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_Management.Domain
+{
+    public class LeaveType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DefaultDay { get; set; }
+        public DateTime DateCreated { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
