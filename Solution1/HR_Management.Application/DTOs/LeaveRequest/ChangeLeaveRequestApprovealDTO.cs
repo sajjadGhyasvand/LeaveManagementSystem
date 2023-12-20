afe93948fdf1f0927fdfb1cf46db063f18f5294e
sajjadGh1995@hotmail.com <sajjadGh1995@hotmail.com>
2023-12-20 10:27:08 +0330

    Add Some DTO

---
+++ Solution1/HR_Management.Application/DTOs/LeaveRequest/ChangeLeaveRequestApprovealDTO.cs
@@ -0,0 +1,14 @@
+﻿using HR_Management.Application.DTOs.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_Management.Application.DTOs.LeaveRequest
+{
+    public class ChangeLeaveRequestApprovealDTO : BaseDTO
+    {
+        public bool? Approved { get; set; }
+    }
+}
