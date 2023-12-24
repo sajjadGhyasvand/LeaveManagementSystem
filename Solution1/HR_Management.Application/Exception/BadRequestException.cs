0a40fc1715b54b5a61704d17fd3e0becba5c353c
sajjadGh1995@hotmail.com <sajjadGh1995@hotmail.com>
2023-12-24 16:34:06 +0330

    Add Exception

---
+++ Solution1/HR_Management.Application/Exception/BadRequestException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_Management.Application.Exception
+{
+    public class BadRequestException : ApplicationException
+    {
+        public BadRequestException(string message):base(message) 
+        {
+            
+        }
+    }
+}
---
+++ Solution1/HR_Management.Application/Exception/ValidationException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_Management.Application.Exception
+{
+    public class ValidationException : ApplicationException
+    {
+        public List<string> Errors { get; set; } = new List<string>();
+        public ValidationException(ValidationResult validationResult)
+        {
+            foreach (var err in validationResult.ErrorMessage)
+            {
+                Errors.Add(err.ToString());
+            }
+        }
+    }
+}
