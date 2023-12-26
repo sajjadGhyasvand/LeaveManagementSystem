ef1ba31f955da20e0def623dd4e50eda054e337f
sajjadGh1995@hotmail.com <sajjadGh1995@hotmail.com>
2023-12-26 16:37:57 +0330

    ADd Configuration in Persistence LAyer

---
+++ Solution1/HR_Management.Persistence/PersistenceServicesRegistration.cs
@@ -0,0 +1,33 @@
+﻿using HR_Management.Application.DTOs.LeaveRequest;
+using HR_Management.Application.Persistence.Contracts;
+using HR_Management.Application.Responses;
+using HR_Management.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_Management.Persistence
+{
+    public static class PersistenceServicesRegistration
+    {
+        public static IServiceCollection ConfigurePersistenceServices(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddDbContext<LeaveManagementDBContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("LeaveManagementConnectionString"));
+
+            });
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddScoped<ILeaveTypeRepository, LeaveTypeRepository>();
+            services.AddScoped<ILeaveRequestDTO, LeaveRequestDTO>();
+            services.AddScoped<ILeaveAllocationRepository, LeaveAllocationRepository>();
+
+            return services;
+        }
+    }
+}
