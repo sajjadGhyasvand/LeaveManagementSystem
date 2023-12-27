5ec5dbe548534f9d8e8138432ad2244a4e7a1f68
sajjadGh1995@hotmail.com <sajjadGh1995@hotmail.com>
2023-12-27 16:54:17 +0330

    Add API

---
+++ Solution1/HR_Management.API/Program.cs
@@ -0,0 +1,31 @@
+using HR_Management.Application;
+using HR_Management.Infra_Structure;
+using HR_Management.Persistence;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.ConfigureApplicationServices();
+builder.Services.ConfigureInfrastructureServices(builder.Configuration);
+builder.Services.ConfigurePersistenceServices(builder.Configuration);
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ Solution1/HR_Management.Infra_Structure/InfrastructureServicesRegistration.cs
@@ -0,0 +1,26 @@
+﻿using HR_Management.Application.Contracts.Infrastructure;
+using HR_Management.Application.Contracts.Persistence;
+using HR_Management.Application.DTOs.LeaveRequest;
+using HR_Management.Application.Models;
+using HR_Management.Infra_Structure.Mail;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_Management.Infra_Structure
+{
+    public static class InfrastructureServicesRegistration
+    {
+        public static IServiceCollection ConfigureInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<EmailSetting>(configuration.GetSection("EmailSettings"));
+            services.AddTransient<IEmailSender, EmailSender>();
+
+            return services;
+        }
+    }
+}
