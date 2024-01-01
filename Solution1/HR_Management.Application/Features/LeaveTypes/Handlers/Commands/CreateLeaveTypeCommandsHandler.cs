16f913b59f8582b304c5d40fef51f484c590ed01
sajjadGh1995@hotmail.com <sajjadGh1995@hotmail.com>
2024-01-01 11:12:41 +0330

    Use Base Response

--- Solution1/HR_Management.Application/Features/LeaveTypes/Handlers/Commands/CreateLeaveTypeCommandsHandler.cs
+++ Solution1/HR_Management.Application/Features/LeaveTypes/Handlers/Commands/CreateLeaveTypeCommandsHandler.cs
@@ -5,6 +5,7 @@ using HR_Management.Application.Contracts.Persistence;
 using HR_Management.Domain;
 using MediatR;
 using System.ComponentModel.DataAnnotations;
+using HR_Management.Application.Responses;
 
 namespace HR_Management.Application.Features.LeaveTypes.Handlers.Commands
 {
@@ -19,17 +20,30 @@ namespace HR_Management.Application.Features.LeaveTypes.Handlers.Commands
             _mapper = mapper;
         }
 
-        public async Task<int> Handle(CreateLeaveTypeCommand request, CancellationToken cancellationToken)
+        public async Task<BaseCommandResponse> Handle(CreateLeaveTypeCommand request, CancellationToken cancellationToken)
         {
+            var response = new BaseCommandResponse();
             #region Validations
             var validator = new ILeaveTypeDTOValidator();
             var ValidationResult = await validator.ValidateAsync(request.LeaveTypeDTO);
             if (ValidationResult.IsValid == false)
-                throw new HR_Management.Application.Exception.ValidationException(ValidationResult);
+            {
+                response.Success = false;
+                response.Message = " Creation Filed";
+                response.Errors = ValidationResult.Errors.Select(e=>e.ErrorMessage).ToList();
+                /*throw new HR_Management.Application.Exception.ValidationException(ValidationResult);*/
+               
+            }
+            else
+            {
+                var leaveType = _mapper.Map<LeaveType>(request.LeaveTypeDTO);
+                leaveType = await _leaveTypeRepository.Add(leaveType);
+                response.Success = true;
+                response.Message = " Creation Success";
+                response.Id = leaveType.Id;
+            }
             #endregion
-            var leaveType = _mapper.Map<LeaveType>(request.LeaveTypeDTO);
-            leaveType = await _leaveTypeRepository.Add(leaveType);
-            return leaveType.Id;
+            return response;
         }
     }
 }
--- Solution1/HR_Management.Application/Features/LeaveTypes/Request/Commands/CreateLeaveTypeCommand.cs
+++ Solution1/HR_Management.Application/Features/LeaveTypes/Request/Commands/CreateLeaveTypeCommand.cs
@@ -5,9 +5,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using HR_Management.Application.DTOs.LeaveType;
+using HR_Management.Application.Responses;
 namespace HR_Management.Application.Features.LeaveTypes.Request.Commands
 {
-    public class CreateLeaveTypeCommand : IRequest<int>
+    public class CreateLeaveTypeCommand : IRequest<BaseCommandResponse>
     {
         public CreateLeaveTypeDTO LeaveTypeDTO { get; set; }
     }
